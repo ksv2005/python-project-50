d7173b23adac38b6ac1c5d9ddf33b52a66576791
Woolfer0097 <Woolfer0097@yandex.ru>
2023-11-25 01:05:17 +0300

    Commit #6. step 5 finished. added yaml generator of difference and determinator of file type

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 import argparse
-import json
+from json import load
+from yaml import safe_load
 
 
-# generates plain difference between two json files
-def generate_diff(file1, file2):
+def generate_diff(file1_data, file2_data):
     result = ["{"]
-    with open(file1) as f1:
-        file1_data = json.load(f1)
-    with open(file2) as f2:
-        file2_data = json.load(f2)
     for key in sorted(file1_data):
         if key in file2_data:
             if file1_data[key] == file2_data[key]:
@@ -19,11 +15,39 @@ def generate_diff(file1, file2):
                 result.append(f"\t+ {key}: {file2_data[key]}")
         else:
             result.append(f"\t- {key}: {file1_data[key]}")
+
     for key in sorted(file2_data):
         if key not in file1_data:
             result.append(f"\t+ {key}: {file2_data[key]}")
     result.append("}")
     return "\n".join(result)
+
+
+# generates plain difference between two json files
+def generate_json_diff(file1, file2):
+    with open(file1) as f1:
+        file1_data = load(f1)
+    with open(file2) as f2:
+        file2_data = load(f2)
+    return generate_diff(file1_data, file2_data)
+
+
+# generates plain difference between two yaml files
+def generate_yaml_diff(file1, file2):
+    result = ["{"]
+    with open(file1) as f1:
+        file1_data = safe_load(f1)
+    with open(file2) as f2:
+        file2_data = safe_load(f2)
+
+    return generate_diff(file1_data, file2_data)
+
+
+def getFilesExtension(file1, file2):
+    if file1.split(".")[-1] in ["yml", "yaml"] and file2.split(".")[-1] in ["yml", "yaml"]:
+        return "yaml"
+    else:
+        return "json"
 
 
 def main():
@@ -36,7 +60,10 @@ def main():
     parser.add_argument('-f', '--format',
                         help='set format of output')
     args = parser.parse_args()
-    print(generate_diff(args.first_file, args.second_file))
+    filename1, filename2 = args.first_file, args.second_file
+    print(generate_yaml_diff(filename1, filename2)
+          if getFilesExtension(filename1, filename2) == "yaml"
+          else generate_json_diff(filename1, filename2))
 
 
 if __name__ == '__main__':
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,18 @@
-from gendiff.scripts.gendiff import generate_diff
+from gendiff.scripts.gendiff import generate_json_diff, generate_yaml_diff
 import os
 
 
 # testing plain json difference
-def test_plain_gendiff():
+def test_json_gendiff():
     FILE1_NAME = os.path.abspath("tests/fixtures/file1.json")
     FILE2_NAME = os.path.abspath("tests/fixtures/file2.json")
     FILE_RIGHT_NAME = os.path.abspath("tests/fixtures/right1.txt")
-    assert generate_diff(FILE1_NAME, FILE2_NAME) == open(FILE_RIGHT_NAME).read()
+    assert generate_json_diff(FILE1_NAME, FILE2_NAME) == open(FILE_RIGHT_NAME).read()
+
+
+# testing plain yaml difference
+def test_yaml_gendiff():
+    FILE1_NAME = os.path.abspath("tests/fixtures/file1.yaml")
+    FILE2_NAME = os.path.abspath("tests/fixtures/file2.yaml")
+    FILE_RIGHT_NAME = os.path.abspath("tests/fixtures/right1.txt")
+    assert generate_yaml_diff(FILE1_NAME, FILE2_NAME) == open(FILE_RIGHT_NAME).read()
